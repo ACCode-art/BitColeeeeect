80b481ec18d4baad223abc6e1ca756d3c15600e3
Adam cooke <adam-cooke@live.co.uk>
2021-03-19 15:18:36 +0000

    V1.0.1
     - Search functionality added
    
    - Added dark mode
    
     - Main container redesign
    
     - Main container changes based on selected crypto

--- script.js
+++ script.js
@@ -1,48 +1,76 @@
 const searchResults = document.querySelector(".searchResults");
 const cryptoContainer = document.querySelector(".cryptoContainer");
-let array = [];
-searchResults.addEventListener("click", (e) => {
-  const p = e.target.textContent;
+const input = document.querySelector("input");
 
-  array.push(p);
-  console.log(array);
+let data = [];
 
-  array.map((arr) => {
-    const html = `<p>${arr}</p>`;
+input.addEventListener("keyup", (e) => {
+  const searchString = e.target.value.toLowerCase();
 
-    cryptoContainer.insertAdjacentHTML("beforeend", html);
+  const filtered = data.filter((coin) => {
+    return (
+      coin.name.toLowerCase().includes(searchString) ||
+      coin.symbol.toLowerCase().includes(searchString)
+    );
   });
+  displayCoins(filtered);
 });
 
 async function crypto() {
   const response = await fetch(
     "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
   );
-  const data = await response.json();
+  data = await response.json();
   console.log(data);
-
-  data.map((coin) => {
-    const {
-      name,
-      symbol,
-      current_price: price,
-      market_cap_rank: rank,
-      high_24h: high,
-      low_24h: low,
-    } = coin;
-    const range = high - low;
-    const HTML = `<div class="coinResults hover">
-    <div class="coinResults__left">
-      <p>${name}</p>
-      <p class="symbol">${symbol.toUpperCase()}</p>
-    </div>
-    <div class="coinResults__right">
-      <p class="price">$ ${price}</p>
-    </div>
-  </div>`;
-
-    searchResults.insertAdjacentHTML("beforeend", HTML);
-  });
 }
 
 crypto();
+
+const displayCoins = (coins) => {
+  const HTML = coins
+    .map((coin) => {
+      return `<div class="coinResults hover">
+      <div class="coinResults__left">
+        <img src=${coin.image}/><p>${coin.name}</p>
+        <p class="symbol">${coin.symbol.toUpperCase()}</p>
+      </div>
+      <div class="coinResults__right">
+        <p class="price">$ ${coin.current_price}</p>
+      </div>
+    </div>`;
+    })
+    .join("");
+
+  searchResults.innerHTML = HTML;
+};
+
+searchResults.addEventListener("click", (e) => {
+  const block = e.target.closest(".hover");
+  const left = block.querySelector(".coinResults__left");
+
+  const img = left.querySelector("img").src;
+  const name = left.querySelector("p").textContent;
+
+  const right = block.querySelector(".coinResults__right");
+  const price = right.querySelector(".price").textContent;
+  console.log(price);
+
+  const html = `
+      <div class="coinMore">
+          <div class="coinMore__header">
+            <img
+              src=${img}
+                alt=""
+             />
+            <h1>${name}</h1>
+          </div>
+
+        
+          <p>${price}</p>
+         
+        </div>
+
+  `;
+
+  cryptoContainer.innerHTML = html;
+});
