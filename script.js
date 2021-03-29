7a2d4860313d95f6cd980ff34aceebc867bf7fe2
Adam cooke <adam-cooke@live.co.uk>
2021-03-29 10:49:41 +0100

    V1.0.2
    additional information now shown for each crypto including
    
    - all time high
    - all time low
    - 24hr high
    - 24hr low
    - price difference over 24hr

--- script.js
+++ script.js
@@ -37,12 +37,28 @@ const displayCoins = (coins) => {
       <div class="coinResults__right">
         <p class="price">$ ${coin.current_price}</p>
       </div>
+      <div class="coinResults__displayNone">
+      <p class="priceLow">${coin.low_24h}</p>
+      <p class="priceHigh">${coin.high_24h}</p>
+      <p class="priceDiff">${coin.price_change_24h}</p>
+      <p class="ath">${coin.ath}</p>
+      <p class="atl">${coin.atl}</p>
+    </div>
     </div>`;
     })
     .join("");
 
   searchResults.innerHTML = HTML;
 };
+
+const formatter = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+
+  // These options are needed to round to whole numbers if that's what you want.
+  //minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
+  //maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
+});
 
 searchResults.addEventListener("click", (e) => {
   const block = e.target.closest(".hover");
@@ -53,7 +69,15 @@ searchResults.addEventListener("click", (e) => {
 
   const right = block.querySelector(".coinResults__right");
   const price = right.querySelector(".price").textContent;
-  console.log(price);
+
+  const displayNone = block.querySelector(".coinResults__displayNone");
+  const priceLow = displayNone.querySelector(".priceLow").textContent;
+  const priceHigh = displayNone.querySelector(".priceHigh").textContent;
+  const priceDiff = displayNone.querySelector(".priceDiff").textContent;
+  const ath = displayNone.querySelector(".ath").textContent;
+  const atl = displayNone.querySelector(".atl").textContent;
+
+  console.log(priceHigh);
 
   const html = `
       <div class="coinMore">
@@ -66,7 +90,15 @@ searchResults.addEventListener("click", (e) => {
           </div>
 
         
-          <p>${price}</p>
+          <h1 style="margin-top: 20px">Current price: ${price}</h1>
+          <p style="margin-top: 20px">Price low: $ ${priceLow}</p>
+          <p style="margin-top: 20px">Price high: $ ${priceHigh}</p>
+          <p style="margin-top: 20px">Price difference: $ ${(
+            priceHigh - priceLow
+          ).toFixed(2)}</p>
+          <p class="green" style="margin-top: 20px">All time high: $ ${ath}</p>
+          <p class="red" style="margin-top: 20px">All time low: $ ${atl}</p>
+          
          
         </div>
 
